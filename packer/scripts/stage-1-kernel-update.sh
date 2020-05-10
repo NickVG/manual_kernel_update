e0a8f9c2d0dc63a2d73fe3a9593e2cc7d1a0bead
Nikolay Gorbunov <krot19831983@mail.ru>
2020-05-11 01:37:36 +0300

    fixed packer script

--- packer/scripts/stage-1-kernel-update.sh
+++ packer/scripts/stage-1-kernel-update.sh
@@ -5,10 +5,10 @@ yum install -y http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
 # Install new kernel
 yum --enablerepo elrepo-kernel install kernel-ml -y
 # Remove older kernels (Only for demo! Not Production!)
-rm -f /boot/*3.10*
+sudo rm -f /boot/*3.10*
 # Update GRUB
-grub2-mkconfig -o /boot/grub2/grub.cfg
-grub2-set-default 0
+sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+sudo grub2-set-default 0
 echo "Grub update done."
 # Reboot VM
 shutdown -r now
